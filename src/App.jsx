dad560367150c68331200b4d064864bff24ac1ac
Zulkuf Demirhan <z.demirhan95@gmail.com>
2023-07-17 23:28:09 +0300

    react bootstrap installed and components had just started to create

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import Container from "react-bootstrap/Container"
+import { Button, Stack } from "react-bootstrap"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Budge-App</h1>
-    </>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="outline-primary">Add Expense</Button>
+        <Button variant="primary">Add Budget</Button>
+      </Stack>
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start" }}></div>
+    </Container>
   )
 }
 
