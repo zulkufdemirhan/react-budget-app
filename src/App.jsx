238fdb7ac0cddb744d597410ded9a724b6ea62b6
Zulkuf Demirhan <z.demirhan95@gmail.com>
2023-07-22 01:06:26 +0300

    add expense component, function created

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,27 @@ import Container from "react-bootstrap/Container"
 import { Button, Stack } from "react-bootstrap"
 import BudgetCard from "./components/BudgetCard"
 import AddBudgetModal from "./components/AddBudgetModal"
+import AddExpenseModal from "./components/AddExpenseModal"
 import { useState } from "react"
 import { useBudgets } from "./contexts/BudgetsContext"
 
 function App() {
   const [showAddBudgetModal, setShowAddButgetModal] = useState(false)
+  const [showAddExpenseModal, setShowAddExpenseModal] = useState(false)
+  const [addExpenseModalBudgetId, setAddExpenseModalBudgetId] = useState()
   const { budgets, getBudgetExpenses } = useBudgets()
+
+  function openAddExpenseModal(budgetId) {
+    setShowAddExpenseModal(true)
+    setAddExpenseModalBudgetId(budgetId)
+  }
+
   return (
     <>
       <Container className="my-4">
         <Stack direction="horizontal" gap="2" className="mb-4">
           <h1 className="me-auto">Budgets</h1>
-          <Button variant="outline-primary">Add Expense</Button>
+          <Button variant="outline-primary" onClick={openAddExpenseModal}>Add Expense</Button>
           <Button variant="primary" onClick={() => setShowAddButgetModal(true)}>Add Budget</Button>
         </Stack>
         <div 
@@ -31,16 +40,25 @@ function App() {
             )
             return (
               <BudgetCard 
-              key={budget.id}
-              name={budget.name} 
-              amount={amount}
-              max={budget.max} 
-            />
+                key={budget.id}
+                name={budget.name} 
+                amount={amount}
+                max={budget.max} 
+                onAddExpenseClick={() => openAddExpenseModal(budget.id)}
+              />
             )
           })}
         </div>
       </Container>
-      <AddBudgetModal show={showAddBudgetModal} handleClose={() => setShowAddButgetModal(false)}/>
+      <AddBudgetModal
+        show={showAddBudgetModal} 
+        handleClose={() => setShowAddButgetModal(false)}
+      />
+      <AddExpenseModal
+        show={showAddExpenseModal} 
+        defaultBudgetId={addExpenseModalBudgetId}
+        handleClose={() => setShowAddExpenseModal(false)}
+      />
     </>
   )
 }
--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,7 +1,7 @@
 import { Card, ProgressBar, Button, Stack } from "react-bootstrap"
 import { currencyFormatter } from "../utils"
 
-export default function BudgetCard({ name, amount, max, gray }) {
+export default function BudgetCard({ name, amount, max, gray, onAddExpenseClick }) {
 
   const classNames = []
 
@@ -30,7 +30,7 @@ export default function BudgetCard({ name, amount, max, gray }) {
           max={max}
           now={amount} />
         <Stack direction="horizontal" gap="2" className="mt-4">
-          <Button variant="outline-primary" className="ms-auto">Add Expense</Button>
+          <Button variant="outline-primary" className="ms-auto" onClick={onAddExpenseClick}>Add Expense</Button>
           <Button variant="outline-secondary">View Expenses</Button>
         </Stack>
       </Card.Body>
