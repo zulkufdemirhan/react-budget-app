59f98d25643d9dcc71dffeed8c444d31f90f1960
Zulkuf Demirhan <z.demirhan95@gmail.com>
2023-07-25 01:37:42 +0300

    total budget card component/function created

--- src/components/BudgetCard.jsx
+++ src/components/BudgetCard.jsx
@@ -1,7 +1,7 @@
 import { Card, ProgressBar, Button, Stack } from "react-bootstrap"
 import { currencyFormatter } from "../utils"
 
-export default function BudgetCard({ name, amount, max, gray, onAddExpenseClick }) {
+export default function BudgetCard({ name, amount, max, gray, hideButtons, onAddExpenseClick }) {
 
   const classNames = []
 
@@ -34,10 +34,12 @@ export default function BudgetCard({ name, amount, max, gray, onAddExpenseClick 
             now={amount} 
           />
         )}
-        <Stack direction="horizontal" gap="2" className="mt-4">
+        { !hideButtons && (
+          <Stack direction="horizontal" gap="2" className="mt-4">
           <Button variant="outline-primary" className="ms-auto" onClick={onAddExpenseClick}>Add Expense</Button>
           <Button variant="outline-secondary">View Expenses</Button>
         </Stack>
+        )}
       </Card.Body>
     </Card>
   )
--- src/components/UncategorizedBudgetCard.jsx
+++ src/components/UncategorizedBudgetCard.jsx
@@ -3,11 +3,9 @@ import BudgetCard from "./BudgetCard"
 
 export default function UncategorizedBudgetCard(props) {
   const { getBudgetExpenses } = useBudgets()
-  const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce(
-    (total, expense) => total + expense.amount,
-    0
-  )
+  
+  const amount = getBudgetExpenses(UNCATEGORIZED_BUDGET_ID).reduce((total, expense) => total + expense.amount, 0)
+  
   if (amount === 0) return null
-
   return <BudgetCard amount={amount} name="Uncategorized" gray {...props} />
 }
